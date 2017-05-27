6d20ae9870a86e3019ea8c2611eee7e4e0ac3ae2
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2017-05-27 10:51:12 +0300

    Added the initial game structure.

---
+++ game.js
@@ -0,0 +1,14 @@
+var pong = {
+  start: function () {
+    this.init();
+    this.run();
+  },
+  init: function () {
+    // ... init
+  },
+  run: function () {
+    // ... run
+  }
+}
+
+pong.start();
