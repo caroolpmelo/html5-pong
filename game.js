0b0c7de761fa2fc97bcf0a6449b9ca122e236a30
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2017-05-27 11:50:50 +0300

    Added the initialization logics for the 2D drawing context.

--- game.js
+++ game.js
@@ -1,13 +1,26 @@
 var pong = {
+  canvas: undefined,
+  ctx: undefined,
   start: function () {
     this.init();
     this.run();
   },
   init: function () {
-    // ... init
+    // get a reference to the target <canvas> element.
+    this.canvas = document.getElementById("game-canvas");
+    if (!this.canvas) {
+      throw Error("Unable to find the canvas element.");
+    }
+
+    // get a reference to the 2D drawing context.
+    this.ctx = this.canvas.getContext("2d");
+    if (!this.ctx) {
+      throw Error("Unable to get 2D draw context from the canvas");
+    }
   },
   run: function () {
-    // ... run
+    // ...
+    this.ctx.fillRect(0,0,800,600);
   }
 }
 
