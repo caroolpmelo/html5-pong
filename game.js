3e236f0626ee07e26e4c30cf29662f4533502fce
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2017-05-28 11:17:59 +0300

    A complete rewrite of the game to follow the revealing module pattern.

--- game.js
+++ game.js
@@ -1,125 +1,216 @@
-var pong = {
-  start: function () {
-    this.init();
-    this.run();
-  },
-  init: function () {
-    // get a reference to the target <canvas> element.
-    this.canvas = document.getElementById("game-canvas");
-    if (!this.canvas) {
-      throw Error("Unable to find the canvas element.");
+var pong = (function () {
+
+  var KEY_ENTER = 13;
+
+  var canvas;
+  var canvasCenter;
+  var ctx;
+  var scene;
+
+  // ==========================================================================
+  /**
+   * The welcoming scene for the Pong game.
+   *
+   * This scene is the introduction scene which will be show to the users when
+   * they open the page. Scene contains the topic of the game, instructions
+   * about how to move paddles with the keyboard controls and a text which
+   * contains an instruction about how to start the game.
+   */
+  var welcomeScene = (function () {
+    /** A function that is called when the game enters the scene */
+    function enter() {
+      document.addEventListener("keyup", onKeyUp);
     }
 
-    // get a reference to the 2D drawing context.
-    this.ctx = this.canvas.getContext("2d");
-    if (!this.ctx) {
-      throw Error("Unable to get 2D draw context from the canvas");
+    /** A function that is called when the game exits the scene. */
+    function exit() {
+      document.removeEventListener("keyup", onKeyUp);
     }
 
-    this.ctx.fillStyle = "white";
-    this.ctx.textAlign = "center";
+    /** A function that is called on each main loop iteration.  */
+    function update() {
+      // ... no implementation required
+    }
 
-    // set the welcome scene as the initial scene.
-    this.setScene(this.scenes.welcome);
-  },
-  run: function () {
-    // update and draw the current scene.
-    this.scene.update();
-    this.scene.draw(this.ctx);
+    /** A function that is called on each rendering frame iteration. */
+    function draw() {
+      // clear the current contents from the canvas.
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // perform a trivial main loop iteration.
-    requestAnimationFrame(this.run.bind(this));
-  },
-  setScene: function (scene) {
-    if (scene) {
+      // draw the application name string.
+      ctx.font = "32pt Arial";
+      ctx.fillText("HTML5 PONG", canvasCenter[0], 100);
+
+      // draw the controls string.
+      ctx.font = "18pt Arial";
+      ctx.fillText("Controls for the left player:", canvasCenter[0], 200);
+      ctx.fillText("W and S", canvasCenter[0], 240);
+      ctx.fillText("Controls for the right player:", canvasCenter[0], 300);
+      ctx.fillText("UP-ARROW and DOWN-ARROW", canvasCenter[0], 340);
+
+      // draw the instructions how to proceed text.
+      ctx.fillText("Press [ENTER] to start the match", canvasCenter[0], 500);
+    }
+
+    /**
+     * A key listener to detect when a user key press is released.
+     *
+     * This implementation will detect only enter presses, which will trigger
+     * a new state transition from the welcome scene to the court scene.
+     *
+     * @param {*} event A key release event from the browser.
+     */
+    function onKeyUp(event) {
+      if (event.keyCode == KEY_ENTER) {
+        setScene(courtScene);
+      }
+    }
+
+    return {
+      enter: enter,
+      exit: exit,
+      update: update,
+      draw: draw
+    }
+  })();
+
+  // ==========================================================================
+  /**
+   * The court scene for the Pong application.
+   *
+   * This is the main game scene where the game simulation will be processed.
+   * It contains all the logics required to move game entities and to make the
+   * game act as a "game". This is the biggest scene from all the three scenes.
+   */
+  var courtScene = (function () {
+
+    var wallSize;
+    var boxSize;
+
+    /** A function that is called when the game enters the scene */
+    function enter() {
+      wallSize = [canvas.width, 20];
+      boxSize = [20, 20];
+    }
+
+    /** A function that is called when the game exits the scene. */
+    function exit() {
+      // ...
+    }
+
+    /** A function that is called on each main loop iteration.  */
+    function update() {
+      // ...
+    }
+
+    /** A function that is called on each rendering frame iteration. */
+    function draw() {
+      // clear the current contents from the canvas.
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      // draw the top and bottom wall.
+      ctx.fillRect(0, 0, wallSize[0], wallSize[1]);
+      ctx.fillRect(0, (canvas.height - wallSize[1]), wallSize[0], wallSize[1]);
+
+      // draw the center line with small boxes.
+      var xPosition = (canvasCenter[0] - boxSize[0] / 2);
+      for (var y = wallSize[1]; y < canvas.height; y += (1.93 * boxSize[0])) {
+        ctx.fillRect(xPosition, y, boxSize[0], boxSize[1]);
+      }
+    }
+
+    return {
+      enter: enter,
+      exit: exit,
+      update: update,
+      draw: draw
+    }
+
+  })();
+
+  // ==========================================================================
+
+  /**
+   * Set the given scene as the active scene.
+   *
+   * Old active scene (if any) will be first exited by calling the exit
+   * function so it can clean up all necessary resources as required. New
+   * scene will be assigned as the active scene and the enter function will
+   * be called to ensure that the new scene can initialize itself.
+   *
+   * @param {*} newScene A new scene to be applied as the active scene.
+   */
+  function setScene(newScene) {
+    if (newScene) {
       // perform a cleanup for the old scene (if defined).
-      if (this.scene) {
-        this.scene.exit();
+      if (scene) {
+        scene.exit();
       }
 
       // apply the new scene and call the scene init (reset).
-      this.scene = scene;
-      this.scene.enter();
-    }
-  },
-  scenes: {
-    welcome: {
-      enter: function (parent) {
-        document.addEventListener("keyup", this.onKeyUp);
-      },
-      exit: function () {
-        document.removeEventListener("keyup", this.onKeyUp);
-      },
-      update: function () {
-        // ... no implementatio required.
-      },
-      draw: function (ctx) {
-        // clear the current contents from the canvas.
-        ctx.clearRect(0, 0, 800, 600);
-
-        // draw the application name string.
-        ctx.font = "32pt Arial";
-        ctx.fillText("HTML5 PONG", 400, 100);
-
-        // draw the controls string.
-        ctx.font = "18pt Arial";
-        ctx.fillText("Controls for the left player:", 400, 200)
-        ctx.fillText("W and S", 400, 240);
-        ctx.fillText("Controls for the right player:", 400, 300);
-        ctx.fillText("UP-ARROW and DOWN-ARROW", 400, 340);
-
-        // draw the instructions how to proceed text.
-        ctx.fillText("-- Press [ENTER] to start the match --", 400, 500);
-      },
-      onKeyUp: function (event) {
-        // transition to court scene after an enter press.
-        if (event.keyCode == 13) {
-          pong.setScene(pong.scenes.court);
-        }
-      }
-    },
-    court: {
-      WALL_HEIGHT: 20,
-      SMALL_BOX_EDGE_LENGTH: 20,
-      enter: function () {
-        // ... scene initialization logic
-      },
-      exit: function () {
-        // ... scene cleanup logic
-      },
-      update: function () {
-        // ... scene update logic
-      },
-      draw: function (ctx) {
-        // clear the current contents from the canvas.
-        ctx.clearRect(0, 0, 800, 600);
-
-        // draw the top and bottom wall.
-        ctx.fillRect(0, 0, 800, this.WALL_HEIGHT);
-        ctx.fillRect(0, (600 - this.WALL_HEIGHT), 800, this.WALL_HEIGHT);
-
-        // draw the center line with small boxes.
-        var xPosition = (400 - this.SMALL_BOX_EDGE_LENGTH / 2);
-        for (var y = this.WALL_HEIGHT; y < 600; y += (1.93 * this.SMALL_BOX_EDGE_LENGTH)) {
-          ctx.fillRect(xPosition, y, this.SMALL_BOX_EDGE_LENGTH, this.SMALL_BOX_EDGE_LENGTH);
-        }
-      }
-    },
-    result: {
-      enter: function () {
-        // ... scene initialization logic
-      },
-      exit: function () {
-        // ... scene cleanup logic
-      },
-      update: function () {
-        // ... scene update logic
-      },
-      draw: function (ctx) {
-        ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-      }
+      scene = newScene;
+      scene.enter();
     }
   }
-}
+
+  /**
+   * Initialize the game.
+   *
+   * Initialization will ensure that the game will get a reference to the
+   * 2D drawing context from the game canvas element. It also provides a
+   * way to define a game wide initializations for scenes etc.
+   */
+  function init() {
+    // get a referene to the target <canvas> element.
+    canvas = document.getElementById("game-canvas");
+    if (!canvas) {
+      throw Error("Unable to find the required canvas element.");
+    }
+
+    // precalculate the center point of the canvas.
+    canvasCenter = [];
+    canvasCenter[0] = (canvas.width / 2);
+    canvasCenter[1] = (canvas.height / 2);
+
+    // get a reference to the 2D drawing context.
+    ctx = canvas.getContext("2d");
+    if (!ctx) {
+      throw Error("Unable to get 2D draw context from the canvas.");
+    }
+
+    // specify global draw definitions.
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+
+    // set the welcome scene as the initial scene.
+    setScene(welcomeScene);
+  }
+
+  function run() {
+    // update and draw the current scene.
+    scene.update();
+    scene.draw();
+
+    // perform a trivial main loop iteration.
+    requestAnimationFrame(run);
+  }
+
+  /**
+   * Start the game.
+   *
+   * Game will be first initialized and the started. Game will be using a
+   * infinite loop (via requestAnimationFrame) as the main loop, so the game
+   * will not will run until the user closes the browser tab or if an error
+   * is detected by the JavaScript.
+   */
+  function start() {
+    init();
+    run();
+  }
+
+  return {
+    start: start
+  };
+})();
 
 pong.start();
